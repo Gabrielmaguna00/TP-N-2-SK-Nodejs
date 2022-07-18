ab495ae2d0f8ffaf9325d599350bbf93bc889906
Gabriel Maguna <gabimaguna@gmail.com>
2022-07-18 17:43:46 -0300

    integracion de conexion de database

---
+++ src/databaseDB.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql')
+
+const mySqlConnection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password:"",
+    database: "latablita"
+})
+
+mySqlConnection.connect((err)=>{
+    if (err) {
+        console.log(err)
+    }
+    else {
+        console.log("Entraste a la matrix")
+    }
+})
+
+module.exports = mySqlConnection
