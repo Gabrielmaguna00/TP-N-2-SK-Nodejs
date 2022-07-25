143ed6733c9865479e97776fb700c1761ed482e3
Gabriel Maguna <gabimaguna@gmail.com>
2022-07-25 16:46:12 -0300

    cambios

--- src/server.js
+++ src/server.js
@@ -6,20 +6,27 @@ const path = require("path");
 const stocks = require("./Routes/Stock");
 const staff = require("./Routes/staff");
 const position = require("./Routes/position");
-// app.engine("html", require("pug").renderFile);
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "pug");
-// app.use(express.json());
-// app.use(express.static("./views"));
-// app.use(express.static("./css"));
 
+app.use(express.urlencoded({ extended: true }));
 app.set("port", process.env.PORT || 3000);
 app.use(morgan("tiny"));
 app.use(express.json());
+
 app.use("/stock", stocks);
 app.use("/staff", staff);
-app.use("/", position)
+app.use("/position", position);
 
+// Motor de plantillas
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static("./views"));
+
+app.get("/", (req, res) => {
+  res.render("index.pug");
+});
+app.get("*", (req, res) => {
+  res.render("404.pug");
+});
 app.listen(app.get("port"), () => {
   console.log("Servidor en el puerto 3000"), app.get("port");
 });
